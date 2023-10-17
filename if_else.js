466093bbcbb3cea66c6476eeb346f97181fb7063
universalkingofooty <heavenridersakthi@gmail.com>
2023-10-17 15:43:59 +0530

    if else home work

---
+++ if_else.js
@@ -0,0 +1,9 @@
+let name="sakthi"
+let age=11
+
+if (age>=18){
+    console.log(`${name} 🚗🚗🚗you can drive a vehicle cause your age is ${age}🚗🚗🚗`);
+}
+else{
+    console.log(`${name} 🚗🚗🚗you can't drive a vehicle cause your age is ${age}🚗🚗🚗`);
+}

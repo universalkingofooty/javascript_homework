5fc1ad591e89788b7919a3a6569c3d6e68dea96a
universalkingofooty <heavenridersakthi@gmail.com>
2023-10-28 20:52:54 +0530

    lcu

---
+++ lcu.js
@@ -0,0 +1,14 @@
+let LCU = ["kaithi","vikram","leo"]
+
+let movie1="kaithi"
+let movie2="vikram"
+let movie3="leo"
+let movie4="rrr"
+
+const movie_name=prompt("enter the movie name")
+
+if (LCU .includes (movie_name)){
+    console.log(`yes the ${movie_name}  movie is in LCU`);
+}else{
+    console.log(`oops the ${movie_name} movie is not in the lcu`);
+}

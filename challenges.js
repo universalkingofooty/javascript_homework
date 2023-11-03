85d9420f6d06eab01b1a367758b8c0b8fd8f0255
universalkingofooty <heavenridersakthi@gmail.com>
2023-11-03 15:00:10 +0530

    hw and challenge

--- challenges.js
+++ challenges.js
@@ -1,28 +1,45 @@
-//1st challenge
-const mark_weight = 48
-const mark_height = 1.47
-const john_weight = 85
-const john_height = 1.90
+// //1st challenge
+// const mark_weight = 48
+// const mark_height = 1.47
+// const john_weight = 85
+// const john_height = 1.90
 
-const mark_bmi = mark_weight / mark_height ** 2
-const john_bmi = john_weight / john_height ** 2
+// const mark_bmi = mark_weight / mark_height ** 2
+// const john_bmi = john_weight / john_height ** 2
 
-console.log("mark mass is:", mark_bmi)
+// console.log("mark mass is:", mark_bmi)
 
-console.log("john mass is:", john_bmi)
+// console.log("john mass is:", john_bmi)
 
-const mark_higher_bmi = mark_bmi > john_bmi
+// const mark_higher_bmi = mark_bmi > john_bmi
 
-console.log(mark_higher_bmi)
+// console.log(mark_higher_bmi)
 
-//second challenge
+// //second challenge
 
-if (mark_bmi > john_bmi) {
-    console.log(`mark's BMI (${(mark_bmi)}) is higher than john's BMI (${john_bmi})`)
+// if (mark_bmi > john_bmi) {
+//     console.log(`mark's BMI (${(mark_bmi)}) is higher than john's BMI (${john_bmi})`)
 
+// }
+// else {
+//     console.log(`john''s BMI ${john_bmi} is higher than mark's BMI ${mark_bmi}`)
+// }
+
+
+//third challenge
+const dolphine_score=(100 + 180 + 89) / 3
+const koalas_score=(100 + 180 + 89) / 3
+console.log(dolphine_score);
+console.log(koalas_score);
+
+if(dolphine_score > koalas_score && dolphine_score >= 100){
+    console.log('dolphine won the game🥳🥳');
+
+}else if (koalas_score > dolphine_score && koalas_score >= 100){
+    console.log('koalas won the game🥳🥳');
+
+}else if(koalas_score == dolphine_score && koalas_score >= 100 && dolphine_score >= 100){
+    console.log('both are won the game🥳🥳');
+}else{
+    console.log('no one won the game😒😒');
 }
-else {
-    console.log(`john''s BMI ${john_bmi} is higher than mark's BMI ${mark_bmi}`)
-}
-
-
--- operator.js
+++ operator.js
@@ -1,47 +1,47 @@
-//OPERATORS
+// //OPERATORS
 
-//ASSAIGNMEN OPERATORS
-let x = 10
-let y = 20
+// //ASSAIGNMEN OPERATORS
+// let x = 10
+// let y = 20
 
-//ARITHMETIC OPERATOR
-//ADD
-result = x + y
-console.log(result)
-//sub
-result = x - y
-console.log(result);
-//MULTY
-result = x * y
-console.log(result);
-//EXPONENTION
-result = x ** y
-console.log(result);
-//divition
-result = x / y
-console.log(result);
-//modules
-result = x % y
-console.log(result);
-//increment
-console.log(++x);
-//decrement
-console.log(--x);
+// //ARITHMETIC OPERATOR
+// //ADD
+// result = x + y
+// console.log(result)
+// //sub
+// result = x - y
+// console.log(result);
+// //MULTY
+// result = x * y
+// console.log(result);
+// //EXPONENTION
+// result = x ** y
+// console.log(result);
+// //divition
+// result = x / y
+// console.log(result);
+// //modules
+// result = x % y
+// console.log(result);
+// //increment
+// console.log(++x);
+// //decrement
+// console.log(--x);
 
-//COMPARISION OPERATOR
-let a = 5
-let b = 5
-console.log(a == b);
-let p = 5
-let q = "5"
-console.log(p === q);
-console.log(a != b);
-console.log(p !== q);
-console.log(x > y);
-console.log(x < y);
-console.log(x <= y);
-console.log(x >= y);
+// //COMPARISION OPERATOR
+// let a = 5
+// let b = 5
+// console.log(a == b);
+// let p = 5
+// let q = "5"
+// console.log(p === q);
+// console.log(a != b);
+// console.log(p !== q);
+// console.log(x > y);
+// console.log(x < y);
+// console.log(x <= y);
+// console.log(x >= y);
 
-//logical operator
-console.log(x < y && y > x);
-console.log(x < y || x > y);
+// //logical operator
+// console.log(x < y && y > x);
+// console.log(x < y || x > y);
